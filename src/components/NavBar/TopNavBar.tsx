b3301b02c07a44ff1867164688e3f8577d3d9f33
cleverakanimoh <cleverakanimoh02@gmail.com>
2024-11-01 10:18:00 +0100

    changed 14 files

--- src/components/NavBar/TopNavBar.tsx
+++ src/components/NavBar/TopNavBar.tsx
@@ -5,9 +5,6 @@ import Logo from "./Logo";
 import { SignedIn, SignedOut, SignInButton } from "@clerk/clerk-react";
 import { Button } from "../ui/button";
 import { useState } from "react";
-import DropDownMenuWrapper from "../dropDownMenu/dropDownMenuWrapper";
-import MessageContainer from "../messageContainer";
-import { Link } from "react-router-dom";
 
 export const TopNavBar = ({ headerStyle }: { headerStyle: string }) => (
   <header className={clsx(headerStyle, "justify-between border-b pb-2")}>
@@ -36,58 +33,20 @@ export const TopNavBar = ({ headerStyle }: { headerStyle: string }) => (
 );
 
 const NotificationWithIndicator = () => {
-  const notificationMessages = [
-    {
-      id: 1,
-      message:
-        "New employee, James Card, has been added to the payroll system.",
-      time: "2 days ago",
-      read: true,
-    },
-    {
-      id: 2,
-      message:
-        "John Doe's expense report for the last quarter has been approved and processed.",
-      time: "3 days ago",
-      read: true,
-    },
-    {
-      id: 3,
-      message:
-        "Emily Smith's presentation slides for the upcoming conference have been reviewed and finalized.",
-      time: "4 days ago",
-      read: true,
-    },
-    {
-      id: 4,
-      message:
-        "Mark Johnson's access to the confidential documents has been temporarily revoked due to security concerns.",
-      time: "5 days ago",
-      read: true,
-    },
-    {
-      id: 5,
-      message: "Samantha White's request for vacation leave has been approved.",
-      time: "6 days ago",
-      read: true,
-    },
-  ];
   const [showNotificationDropdown, setShowNotificationDropdown] =
     useState(false);
-  const [isNotification, setIsNotification] = useState("");
 
   return (
     <>
       <Button
         className="relative p-0 h-fit cursor-pointer shadow-none notification"
-        onClick={(e: any) => {
-          console.log(showNotificationDropdown, isNotification);
-
-          setShowNotificationDropdown(!showNotificationDropdown);
-          setIsNotification(e.target.className);
+        onClick={() => {
+          setShowNotificationDropdown((prev) => !prev);
         }}
       >
-        <span className="size-1.5 bg-red-600 absolute -right-1 top-0 rounded-full" />
+        {showNotificationDropdown && (
+          <span className="size-1.5 bg-red-600 absolute -right-1 top-0 rounded-full" />
+        )}
         <NotificationIcon />
       </Button>
     </>
