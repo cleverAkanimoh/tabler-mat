97aea1a7f3ca20624b753485b64ff265718a42c2
cleverakanimoh <cleverakanimoh02@gmail.com>
2024-11-01 13:19:05 +0100

    modified index.tsx in Dashboard

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -22,12 +22,32 @@ const Dashboard = () => {
     <div className="grid gap-4 container">
       <h1 className="text-gray-600">Dashboard</h1>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4">
-        <StatsCard title="New Tickets" value="43" change="+6%" />
-        <StatsCard title="Closed Today" value="17" change="-3%" />
-        <StatsCard title="New Replies" value="7" change="+9%" />
-        <StatsCard title="Followers" value="27.3k" change="+3%" />
+        <StatsCard
+          title="New Tickets"
+          value={loading ? "0" : `${data[0]?.value}`}
+          change="+6%"
+        />
+        <StatsCard
+          title="Closed Today"
+          value={loading ? "0" : `${data[1]?.value}`}
+          change="-3%"
+        />
+        <StatsCard
+          title="New Replies"
+          value={loading ? "0" : `${data[3]?.value}`}
+          change="+9%"
+        />
+        <StatsCard
+          title="Followers"
+          value={loading ? "0" : "27.3k"}
+          change="+3%"
+        />
         <StatsCard title="Daily Earnings" value="$95" change="-2%" />
-        <StatsCard title="Products" value="621" change="-1%" />
+        <StatsCard
+          title="Products"
+          value={loading ? "0" : `${data[5]?.value}`}
+          change="-1%"
+        />
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
         <DevelopmentActivity />
